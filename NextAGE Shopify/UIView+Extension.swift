61dc195c5c5738844ca51208dd9140193825337e
engy <132174880+GegeSaad@users.noreply.github.com>
2024-09-01 23:27:59 +0300

    initial structure for project

---
+++ NextAGE Shopify/UIView+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+Extension.swift
+//  NextAGE Shopify
+//
+//  Created by Engy on 9/1/24.
+//
+
+import UIKit
+extension UIView {
+    func addCornerRadius(radius:CGFloat){
+        self.layer.cornerRadius = radius
+
+    }
+
+}
