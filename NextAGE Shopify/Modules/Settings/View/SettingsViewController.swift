f1784646ee127c8f4e65ba5afbfea459e0be3d8c
M7Aymn <modyhemead@gmail.com>
2024-09-02 21:46:34 +0300

    Initial Settings View

--- NextAGE Shopify/Modules/Settings/View/SettingsViewController.swift
+++ NextAGE Shopify/Modules/Settings/View/SettingsViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    let settings = [
+        ("Address", "mappin.and.ellipse.circle"),
+        ("Currency", "coloncurrencysign.circle"),
+        ("About", "info.circle")
+    ]
 
+    
+    @IBOutlet weak var settingsTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        title = "Settings"
+        
+        settingsTableView.delegate = self
+        settingsTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
@@ -25,5 +36,32 @@ class SettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func logoutButton(_ sender: Any) {
+        print("Logout")
+    }
+    
+}
 
+extension SettingsViewController: UITableViewDelegate {
+    
+}
+
+extension SettingsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SettingCell
+        cell.config(label: settings[indexPath.row].0, imageName: settings[indexPath.row].1)
+        return cell
+    }
+    
+    
+}
+
+extension SettingsViewController: UICollectionViewDelegateFlowLayout {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 74
+    }
 }
