9c5d2c9733fbfff18ce8ddb1d7ed0792eab871c8
M7Aymn <modyhemead@gmail.com>
2024-09-03 16:06:13 +0300

    Added currency and about view + logout button action

--- NextAGE Shopify/Modules/Settings/View/SettingsViewController.swift
+++ NextAGE Shopify/Modules/Settings/View/SettingsViewController.swift
@@ -37,13 +37,36 @@ class SettingsViewController: UIViewController {
     }
     */
     @IBAction func logoutButton(_ sender: Any) {
-        print("Logout")
+        let alert = UIAlertController(title: "Logout?", message: "Are you sure you want to logout?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { _ in
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let window = windowScene.windows.first {
+                let story = UIStoryboard(name: "Main", bundle:nil)
+                let vc = story.instantiateViewController(withIdentifier: "MainNavigationController")
+                window.rootViewController = vc
+                window.makeKeyAndVisible()
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel))
+        self.present(alert, animated: true)
     }
     
 }
 
 extension SettingsViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Settings", bundle: nil)
+        let vc: UIViewController
+        switch indexPath.row {
+        case 0:
+            vc = storyboard.instantiateViewController(withIdentifier: "CurrencyViewController")
+        case 1:
+            vc = storyboard.instantiateViewController(withIdentifier: "CurrencyViewController")
+        default:
+            vc = storyboard.instantiateViewController(withIdentifier: "AboutViewController")
+        }
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
 extension SettingsViewController: UITableViewDataSource {
