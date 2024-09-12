8a345ce408897b43073a6a0decbd4b392e3481b8
AngelSaad0 <engysaad573new@gmail.com>
2024-09-13 01:03:54 +0300

    AllOrders ViewModel

--- NextAGE Shopify/Modules/Orders/ViewModel/AllOrdersViewModel.swift
+++ NextAGE Shopify/Modules/Orders/ViewModel/AllOrdersViewModel.swift
@@ -2,7 +2,50 @@
 //  AllOrdersViewModel.swift
 //  NextAGE Shopify
 //
-//  Created by Mohamed Ayman on 08/09/2024.
+//  Created by Engy on 08/09/2024.
 //
 
 import Foundation
+
+class AllOrdersViewModel {
+    // MARK: - Properties
+    let networkManager: NetworkManagerProtocol
+    let userDefaultManager: UserDefaultManager
+    var orders: [Order] = []
+    var setIndicator: (Bool)->() = {_ in}
+    var showMessage: (String)->() = {_ in}
+    var displayMessage: (VaildMassage, Bool)->() = {_, _ in}
+    var removeMessage: ()->() = {}
+    var bindResultToTableView: ()->() = {}
+    
+    // MARK: - Initializer
+    init() {
+        networkManager = NetworkManager.shared
+        userDefaultManager = UserDefaultManager.shared
+    }
+    
+    // Public Methods
+    func fetchAllOrders(completion: @escaping ([Order]?)->()) {
+        networkManager.fetchData(from: ShopifyAPI.orders.shopifyURLString(), responseType: Orders.self, headers: []) { result in
+            completion(result?.orders)
+        }
+    }
+    
+    func updateUserOrders() {
+        setIndicator(true)
+        fetchAllOrders { orders in
+            self.setIndicator(false)
+            guard let orders = orders else {
+                self.displayMessage(.ordersFetchingFailed, true)
+                return
+            }
+            self.orders = orders.filter {$0.customer.id == self.userDefaultManager.customerID}
+            if self.orders.count == 0 {
+                self.showMessage("No Orders Yet ")
+            } else {
+                self.removeMessage()
+            }
+            self.bindResultToTableView()
+        }
+    }
+}
