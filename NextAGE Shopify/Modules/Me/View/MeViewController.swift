e6cfa151f0da42180b967d5496fb7673db823cda
AngelSaad0 <engysaad573new@gmail.com>
2024-09-13 00:26:08 +0300

    added MeViewModel

--- NextAGE Shopify/Modules/Me/View/MeViewController.swift
+++ NextAGE Shopify/Modules/Me/View/MeViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class MeViewController: UIViewController {
+
     // MARK: -  @IBOutlet
     @IBOutlet var logInStack: UIStackView!
     @IBOutlet weak var userGreetingLabel: UILabel!
@@ -18,121 +19,102 @@ class MeViewController: UIViewController {
     @IBOutlet var logInButton: UIButton!
 
     // MARK: -  Properties
-    private var networkManager:NetworkManager
-    private var userDefaultsManager:UserDefaultManager
-    private var connectivityService:ConnectivityServiceProtocol
-    let ordersIndicator = UIActivityIndicatorView(style: .large)
-    let wishlistIndicator = UIActivityIndicatorView(style: .large)
-    var orders: [Order] = []
-    var wishlist: [LineItem] = []
-    var customerName: String?
-    var isUserLoggedIn: Bool?
-    var currentCustomerId: Int?
+    private var viewModel: MeViewModel!
+    private let ordersIndicator = UIActivityIndicatorView(style: .large)
+    private let wishlistIndicator = UIActivityIndicatorView(style: .large)
 
     // MARK: -  View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViewModel()
+        setupUI()
         checkInternetConnection()
-        updateUI()
     }
+
     override func viewWillAppear(_ animated: Bool) {
-        if isUserLoggedIn ?? false {
-            updateUserOrders()
-            loadWishlistData()
+        super.viewWillAppear(animated)
+        if viewModel.isUserLoggedIn {
+            viewModel.updateUserOrders()
+            viewModel.loadWishlistData()
         }
     }
 
-    // MARK: - Required init
-    required init?(coder: NSCoder) {
-        networkManager = NetworkManager()
-        userDefaultsManager = UserDefaultManager.shared
-        connectivityService = ConnectivityService.shared
-        super.init(coder: coder)
+    private func setupViewModel() {
+        viewModel = MeViewModel(networkManager: NetworkManager(), userDefaultsManager: UserDefaultManager.shared, connectivityService: ConnectivityService.shared)
+
+        viewModel.onOrdersUpdated = { [weak self] in
+            self?.updateOrdersUI()
+        }
+
+        viewModel.onWishlistUpdated = { [weak self] in
+            self?.updateWishlistUI()
+        }
+
+        viewModel.onInternetConnectionChecked = { [weak self] in
+            self?.updateUIForLoginState()
+        }
+
+        viewModel.onShowNoInternetAlert = { [weak self] in
+            self?.showNoInternetAlert()
+        }
     }
-    // MARK: -  private Method
-    private func updateUI() {
-        isUserLoggedIn = userDefaultsManager.isLogin
-        currentCustomerId = userDefaultsManager.customerID
-        customerName = userDefaultsManager.name == "" ? nil : userDefaultsManager.name
+
+    private func setupUI() {
         registerButton.addCornerRadius(radius: 12)
         logInButton.addCornerRadius(radius: 12)
         wishlistCollectionView.register(UINib(nibName: "CategoriesCollectionCell", bundle: nil), forCellWithReuseIdentifier: "CategoriesCollectionCell")
         ordersListTableView.register(UINib(nibName: "OrdersTableViewCell", bundle: nil), forCellReuseIdentifier: "OrdersTableViewCell")
         setupIndicators()
     }
+
     private func setupIndicators() {
         ordersIndicator.center = CGPoint(x: view.center.x, y: ordersListTableView.center.y + 80)
         view.addSubview(ordersIndicator)
         wishlistIndicator.center = CGPoint(x: view.center.x, y: wishlistCollectionView.center.y + 80)
         view.addSubview(wishlistIndicator)
     }
+
     private func checkInternetConnection() {
-            connectivityService.checkInternetConnection { [weak self] isConnected in
-                guard let self = self else { return }
-                if isConnected {
-                    self.updateUIForLoginState()
-                } else {
-                    self.showNoInternetAlert()
-                }
-            }
-        }
-     private func updateUIForLoginState() {
-        if isUserLoggedIn ?? false {
+        viewModel.checkInternetConnection()
+    }
+
+    private func updateUIForLoginState() {
+        if viewModel.isUserLoggedIn {
             logInStack.isHidden = false
             notLoggedInView.isHidden = true
-            userGreetingLabel.text = "Welcome \(customerName ?? "Sir")"
-            updateUserOrders()
-            loadWishlistData()
+            userGreetingLabel.text = "Welcome \(viewModel.customerName ?? "Sir")"
         } else {
             notLoggedInView.isHidden = false
             logInStack.isHidden = true
         }
     }
-    private func updateUserOrders() {     
-        ordersIndicator.startAnimating()
-        fetchAllOrders { orders in
-            self.ordersIndicator.stopAnimating()
-            guard let orders = orders else {
-                self.ordersListTableView.displayEmptyMessage("No Orders Yet ")
-                displayMessage(massage: .ordersFetchingFailed, isError: true)
-                return
-            }
-            self.orders = orders.filter {$0.customer.id == self.currentCustomerId}
-            if (self.orders.count == 0) {
-                self.ordersListTableView.displayEmptyMessage("No Orders Yet ")
-            } else {
-                self.ordersListTableView.removeEmptyMessage()
-            }
-            self.ordersListTableView.reloadData()
+
+    private func updateOrdersUI() {
+        ordersIndicator.stopAnimating()
+        if viewModel.orders.isEmpty {
+            ordersListTableView.displayEmptyMessage("No Orders Yet ")
+        } else {
+            ordersListTableView.removeEmptyMessage()
         }
+        ordersListTableView.reloadData()
     }
-    private func loadWishlistData() {
-        wishlistIndicator.startAnimating()
-        networkManager.fetchData(from: ShopifyAPI.draftOrder(id: userDefaultsManager.wishlistID).shopifyURLString(), responseType: DraftOrderWrapper.self) { result in
-            self.wishlist = result?.draftOrder.lineItems ?? []
-            if self.wishlist.first?.variantID == nil {
-                self.wishlist = Array(self.wishlist.dropFirst())
-            }
-            self.wishlistIndicator.stopAnimating()
-            if self.wishlist.count == 0 {
-                self.wishlistCollectionView.displayEmptyMessage("Add some products to your wishlist ")
-            } else {
-                self.wishlistCollectionView.removeEmptyMessage()
-            }
-            self.wishlistCollectionView.reloadData()
-            
+
+    private func updateWishlistUI() {
+        wishlistIndicator.stopAnimating()
+        if viewModel.wishlist.isEmpty {
+            wishlistCollectionView.displayEmptyMessage("Add some products to your wishlist ")
+        } else {
+            wishlistCollectionView.removeEmptyMessage()
         }
+        wishlistCollectionView.reloadData()
     }
-    private func fetchAllOrders(completion: @escaping ([Order]?)->()) {
-        networkManager.fetchData(from: ShopifyAPI.orders.shopifyURLString(), responseType: Orders.self) { result in
-            completion(result?.orders)
-        }
-    }
+
+
     // MARK: - IBActions
     @IBAction func viewAllOrdersClicked(_ sender: UIButton) {
         pushViewController(vcIdentifier: "AllOrdersViewController", withNav: navigationController)
-
     }
+
     @IBAction func viewAllWishListClicked(_ sender: UIButton) {
         pushViewController(vcIdentifier: "WishlistViewController", withNav: navigationController)
     }
@@ -140,12 +122,10 @@ class MeViewController: UIViewController {
     @IBAction func logInButtonClicked(_ sender: UIButton) {
         pushViewController(vcIdentifier: "SignInViewController", withNav: navigationController)
     }
+
     @IBAction func registerButtonClicked(_ sender: UIButton) {
         pushViewController(vcIdentifier: "SignUpViewController", withNav: navigationController)
-
-
     }
-
 }
 
 // MARK: - TableView DataSource & Delegate
@@ -153,11 +133,12 @@ class MeViewController: UIViewController {
 extension MeViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return min(orders.count, 2)
+        return min(viewModel.orders.count, 2)
     }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "OrdersTableViewCell", for: indexPath) as! OrdersTableViewCell
-        cell.configure(with: orders[indexPath.row])
+        cell.configure(with: viewModel.orders[indexPath.row])
         return cell
     }
 
@@ -169,28 +150,28 @@ extension MeViewController: UITableViewDelegate, UITableViewDataSource {
         return 84
     }
 }
+
 // MARK: - CollectionView DataSource & Delegate
+
 extension MeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return wishlist.count
+        return viewModel.wishlist.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoriesCollectionCell", for: indexPath) as! CategoriesCollectionCell
-        cell.configureForWishlist(with: wishlist[indexPath.row])
+        cell.configureForWishlist(with: viewModel.wishlist[indexPath.row])
         return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let productDetailsViewController = storyboard?.instantiateViewController(withIdentifier: "ProductDetailsViewController") as! ProductDetailsViewController
-        productDetailsViewController.productID = wishlist[indexPath.row].productID
+        productDetailsViewController.productID = viewModel.wishlist[indexPath.row].productID
         navigationController?.pushViewController(productDetailsViewController, animated: true)
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width:collectionView.frame.width/2 - 18 , height: collectionView.frame.height)
-
+        return CGSize(width: collectionView.frame.width / 2 - 18, height: collectionView.frame.height)
     }
 }
-
--- NextAGE Shopify/Modules/Me/ViewModel/MeViewModel.swift
+++ NextAGE Shopify/Modules/Me/ViewModel/MeViewModel.swift
@@ -2,7 +2,85 @@
 //  MeViewModel.swift
 //  NextAGE Shopify
 //
-//  Created by Mohamed Ayman on 08/09/2024.
+//  Created by Engy on 08/09/2024.
 //
 
 import Foundation
+
+class MeViewModel {
+
+    // MARK: - Properties
+    private let networkManager: NetworkManagerProtocol
+    private let userDefaultsManager: UserDefaultManager
+    private let connectivityService: ConnectivityServiceProtocol
+
+    var orders: [Order] = []
+    var wishlist: [LineItem] = []
+    var customerName: String?
+    var isUserLoggedIn: Bool = false
+    var currentCustomerId: Int?
+
+    // MARK: - Callbacks
+    var onOrdersUpdated: (() -> Void)?
+    var onWishlistUpdated: (() -> Void)?
+    var onInternetConnectionChecked: (() -> Void)?
+    var onShowNoInternetAlert: (() -> Void)?
+
+    // MARK: - Initialization
+    init(networkManager: NetworkManager, userDefaultsManager: UserDefaultManager, connectivityService: ConnectivityServiceProtocol) {
+        self.networkManager = networkManager
+        self.userDefaultsManager = userDefaultsManager
+        self.connectivityService = connectivityService
+        setupInitialState()
+    }
+
+    private func setupInitialState() {
+        isUserLoggedIn = userDefaultsManager.isLogin
+        currentCustomerId = userDefaultsManager.customerID
+        customerName = userDefaultsManager.name.isEmpty ? nil : userDefaultsManager.name
+    }
+
+    func checkInternetConnection() {
+        connectivityService.checkInternetConnection { [weak self] isConnected in
+            guard let self = self else { return }
+            if isConnected {
+                self.onInternetConnectionChecked?()
+            } else {
+                self.onShowNoInternetAlert?()
+            }
+        }
+    }
+
+    func updateUserOrders() {
+        fetchAllOrders { [weak self] orders in
+            guard let self = self else { return }
+            if let orders = orders {
+                self.orders = orders.filter { $0.customer.id == self.currentCustomerId }
+                self.onOrdersUpdated?()
+            } else {
+                self.orders = []
+                Task { @MainActor in
+                                displayMessage(massage: .ordersFetchingFailed, isError: true)
+                            }
+                self.onOrdersUpdated?()
+            }
+        }
+    }
+
+    func loadWishlistData() {
+        networkManager.fetchData(from: ShopifyAPI.draftOrder(id: userDefaultsManager.wishlistID).shopifyURLString(), responseType: DraftOrderWrapper.self, headers: []) { [weak self] result in
+            guard let self = self else { return }
+            self.wishlist = result?.draftOrder.lineItems ?? []
+            if self.wishlist.first?.variantID == nil {
+                self.wishlist = Array(self.wishlist.dropFirst())
+            }
+            self.onWishlistUpdated?()
+        }
+    }
+
+    private func fetchAllOrders(completion: @escaping ([Order]?)->()) {
+        networkManager.fetchData(from: ShopifyAPI.orders.shopifyURLString(), responseType: Orders.self, headers: []) { result in
+            completion(result?.orders)
+        }
+    }
+}
