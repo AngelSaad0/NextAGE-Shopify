d5c58086d440384595d723c786acc5cfbf8a84d1
a7madelgndy <a7madelgndy@gmail.com>
2024-09-12 14:40:57 +0300

    SignIn ViewModel

--- NextAGE Shopify/Modules/Auth/View/SignInViewController.swift
+++ NextAGE Shopify/Modules/Auth/View/SignInViewController.swift
@@ -8,35 +8,34 @@
 import UIKit
 
 class SignInViewController: UIViewController {
-    // MARK: - IBOutlet
+    // MARK: - IBOutlets
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var passwordView: UIView!
     @IBOutlet var emailView: UIView!
     @IBOutlet var hidePasswordButton: UIButton!
-    // MARK: -  properties
-    var isSecurePasswordText = true
-    private var networkManager: NetworkManager
-    private var userDefaultManager: UserDefaultManager
-    private var connectivityService: ConnectivityServiceProtocol
-    private var email: String?
-    private var password: String?
 
-    // MARK: -  View life cycle
+    // MARK: - Properties
+    private var viewModel: SignInViewModel!
+    private var isSecurePasswordText = true
+
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
-    }
-    // MARK: - Required init
-    required init?(coder: NSCoder) {
-        networkManager = NetworkManager()
-        userDefaultManager = UserDefaultManager.shared
-        connectivityService = ConnectivityService.shared
-        super.init(coder: coder)
+
+        // Initialize ViewModel
+        viewModel = SignInViewModel(
+            networkManager: NetworkManager(),
+            userDefaultManager: UserDefaultManager.shared,
+            connectivityService: ConnectivityService.shared
+        )
+
+        setupBindings()
     }
 
-    // MARK: -  Privte method
+    // MARK: - Private Methods
     private func updateUI() {
         title = "Sign In"
         loginButton.addCornerRadius(radius: 16)
@@ -44,122 +43,52 @@ class SignInViewController: UIViewController {
         passwordView.addCornerRadius(radius: 8)
     }
 
-    private func copyEnteredFields() {
-        email = emailTextField.text?.trimmingCharacters(in: .whitespaces)
-        password = passwordTextField.text?.trimmingCharacters(in: .whitespaces)
-    }
-    
-    private func saveToUserDefaults(customer: CustomerInfo) {
-        userDefaultManager.isLogin = true
-        userDefaultManager.name = (customer.firstName ?? "") + " " + (customer.lastName ?? "")
-        userDefaultManager.email = customer.email ?? ""
-        userDefaultManager.password = customer.tags ?? ""
-        userDefaultManager.customerID = customer.id ?? 0
-        if let draftOrderIDs = customer.note?.components(separatedBy: ","), draftOrderIDs.count == 2 {
-            userDefaultManager.shoppingCartID = Int(draftOrderIDs[0]) ?? 0
-            userDefaultManager.wishlistID = Int(draftOrderIDs[1]) ?? 0
-        }
-        userDefaultManager.storeData()
-    }
-    
-    private func fetchAllCustomers(compilation: @escaping ([CustomerInfo]?) -> Void) {
-        let urlString = ShopifyAPI.customers.shopifyURLString()
-        networkManager.fetchData(from: urlString, responseType: Customers.self) { result in
-            compilation(result?.customers)
-        }
-    }
-    
-    private func validateLoginFields() -> Bool {
-        if emailTextField.text?.trimmingCharacters(in: .whitespaces) == "" {
-            displayMessage(massage: .emailEmpty, isError: true)
-            return false
-        }
-        if !isValidMobileOrEmail(emailTextField.text?.trimmingCharacters(in: .whitespaces) ?? ""){
-            displayMessage(massage: .emailValid, isError: true)
-            return false
-        }
-        if passwordTextField.text?.trimmingCharacters(in: .whitespaces) == "" {
-            displayMessage(massage: .passwordEmpty, isError: true)
-            return false
-        }
-//        if !isValidPassword(passwordTextField.text?.trimmingCharacters(in: .whitespaces) ?? "") {
-//            displayMessage(massage: .passwordVaild, isError: true)
-//            return false
-//        }
-        return true
-    }
-    
-    private func checkUserAuth() {
-        fetchAllCustomers { customers in
-            guard let customers = customers else {
-                displayMessage(massage: .checkingEmailFail, isError: true)
-                return
+    private func setupBindings() {
+        viewModel.onSuccess = {
+            DispatchQueue.main.async {
+                UIWindow.setRootViewController(storyboard: "Main", vcIdentifier: "MainTabBarNavigationController")
+                displayMessage(massage: .successLogin, isError: false)
             }
-            
-            var foundCustomer: CustomerInfo?
-            for customer in customers {
-                if customer.email == self.email?.lowercased() {
-                    foundCustomer = customer
-                    break
-                }
-            }
-            
-            if foundCustomer == nil {
-                displayMessage(massage: .emailDoesNotExist, isError: true)
-            } else {
-                guard foundCustomer!.tags == self.password else {
-                    displayMessage(massage: .passwordDoesNotMatch, isError: true)
-                    return
-                }
-                
-                self.saveToUserDefaults(customer: foundCustomer!)
-                
-                DispatchQueue.main.async {
-                    UIWindow.setRootViewController(storyboard: "Main", vcIdentifier: "MainTabBarNavigationController")
-                    displayMessage(massage: .successLogin, isError: false)
-                }
+        }
+
+        viewModel.onError = {  message in
+            DispatchQueue.main.async {
+                displayMessage(massage: message, isError: true)
             }
         }
     }
-    
-    // MARK: -  Action
+
+    // MARK: - Actions
     @IBAction func hidePasswordButtonClicked(_ sender: UIButton) {
         isSecurePasswordText.toggle()
-        hidePasswordButton.setImage(UIImage(systemName: isSecurePasswordText ? ("eye.slash"):("eye")), for:.normal)
+        hidePasswordButton.setImage(UIImage(systemName: isSecurePasswordText ? "eye.slash" : "eye"), for: .normal)
         passwordTextField.isSecureTextEntry = isSecurePasswordText
-
     }
+
     @IBAction func loginButtonClicked(_ sender: UIButton) {
-        
-        connectivityService.checkInternetConnection { [weak self] isConnected in
+        viewModel.connectivityService.checkInternetConnection{ [weak self] isConnected in
+            guard let self = self else { return }
             if isConnected {
-                if self?.validateLoginFields() ?? false {
-                    self?.copyEnteredFields()
-                    self?.checkUserAuth()
-                }
+                let email = self.emailTextField.text
+                let password = self.passwordTextField.text
+                self.viewModel.validateAndLogin(email: email, password: password)
             } else {
-                self?.showNoInternetAlert()
+                self.showNoInternetAlert()
             }
         }
-        
     }
 
     @IBAction func registerButtonClicked(_ sender: UIButton) {
-        pushViewController(vcIdentifier: "SignUpViewController", withNav: navigationController)
-
+        self.pushViewController(vcIdentifier: "SignUpViewController", withNav: self.navigationController)
     }
 
     @IBAction func forgetPasswordButtonClicked(_ sender: UIButton) {
-#warning("may be delet if api cant do so")
+        // Handle forgot password action if API supports it
     }
-
 
     @IBAction func guestButtonClicked(_ sender: UIButton) {
         UserDefaultManager.shared.continueAsAGuest = true
         UserDefaultManager.shared.storeData()
-        UIWindow.setRootViewController(storyboard:"Main", vcIdentifier : "MainTabBarNavigationController")
-
-
+        UIWindow.setRootViewController(storyboard: "Main", vcIdentifier: "MainTabBarNavigationController")
     }
-
 }
--- NextAGE Shopify/Modules/Auth/ViewModel/SignInViewModel.swift
+++ NextAGE Shopify/Modules/Auth/ViewModel/SignInViewModel.swift
@@ -6,3 +6,87 @@
 //
 
 import Foundation
+
+class SignInViewModel {
+    // MARK: - Properties
+     let networkManager: NetworkManager
+     let userDefaultManager: UserDefaultManager
+     let connectivityService: ConnectivityServiceProtocol
+
+    var onSuccess: (() -> Void)?
+    var onError: ((VaildMassage) -> Void)?
+
+    init(networkManager: NetworkManager, userDefaultManager: UserDefaultManager, connectivityService: ConnectivityServiceProtocol) {
+        self.networkManager = networkManager
+        self.userDefaultManager = userDefaultManager
+        self.connectivityService = connectivityService
+    }
+
+    // MARK: - Public Methods
+    func validateAndLogin(email: String?, password: String?) {
+        guard let email = email?.trimmingCharacters(in: .whitespaces), !email.isEmpty else {
+            onError?(.emailEmpty)
+            return
+        }
+
+        guard isValidMobileOrEmail(email) else {
+            onError?(.emailValid)
+            return
+        }
+
+        guard let password = password?.trimmingCharacters(in: .whitespaces), !password.isEmpty else {
+            onError?(.passwordEmpty)
+            return
+        }
+
+        checkUserAuth(email: email, password: password)
+    }
+
+    private func checkUserAuth(email: String, password: String) {
+        fetchAllCustomers { [weak self] customers in
+            guard let self = self else { return }
+            guard let customers = customers else {
+                self.onError?(.checkingEmailFail)
+                return
+            }
+
+            guard let foundCustomer = customers.first(where: { $0.email == email.lowercased() }) else {
+                self.onError?(.emailDoesNotExist)
+                return
+            }
+
+            if foundCustomer.tags != password {
+                self.onError?(.passwordDoesNotMatch)
+                return
+            }
+
+            self.saveToUserDefaults(customer: foundCustomer)
+            self.onSuccess?()
+        }
+    }
+
+    private func fetchAllCustomers(completion: @escaping ([CustomerInfo]?) -> Void) {
+        let urlString = ShopifyAPI.customers.shopifyURLString()
+        networkManager.fetchData(from: urlString, responseType: Customers.self) { result in
+            completion(result?.customers)
+        }
+    }
+
+    private func saveToUserDefaults(customer: CustomerInfo) {
+        userDefaultManager.isLogin = true
+        userDefaultManager.name = [(customer.firstName ?? ""), (customer.lastName ?? "")].joined(separator: " ")
+        userDefaultManager.email = customer.email ?? ""
+        userDefaultManager.password = customer.tags ?? ""
+        userDefaultManager.customerID = customer.id ?? 0
+        if let draftOrderIDs = customer.note?.components(separatedBy: ","), draftOrderIDs.count == 2 {
+            userDefaultManager.shoppingCartID = Int(draftOrderIDs[0]) ?? 0
+            userDefaultManager.wishlistID = Int(draftOrderIDs[1]) ?? 0
+        }
+        userDefaultManager.storeData()
+    }
+
+    private func isValidMobileOrEmail(_ text: String) -> Bool {
+        // Implement your validation logic here
+        return true
+    }
+}
