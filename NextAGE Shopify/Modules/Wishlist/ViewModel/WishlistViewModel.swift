544e846e58010ad277ebfdf7dbfa3a104633b2e0
a7madelgndy <a7madelgndy@gmail.com>
2024-09-13 04:14:13 +0300

    added WishlistViewModel

--- NextAGE Shopify/Modules/Wishlist/ViewModel/WishlistViewModel.swift
+++ NextAGE Shopify/Modules/Wishlist/ViewModel/WishlistViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+class WishlistViewModel {
+    
+    // MARK: -  Properties
+    let networkManager: NetworkManagerProtocol
+    let userDefaultsManager: UserDefaultManager
+    let connectivityService: ConnectivityServiceProtocol
+    var wishlist: [LineItem] = []
+    var displayEmptyMessage:((String)->Void) = {_ in}
+    var removeEmptyMessage:(()->()) = {}
+    var bindResultToTableView:(()->()) = {}
+    var setIndicator:((Bool)->()) = {_ in}
+    
+    
+    
+    // MARK: -  initalzation
+    init(networkManager: NetworkManager = NetworkManager(),
+         userDefaultsManager: UserDefaultManager = UserDefaultManager.shared,
+         connectivityService: ConnectivityServiceProtocol = ConnectivityService.shared) {
+        self.networkManager = networkManager
+        self.userDefaultsManager = userDefaultsManager
+        self.connectivityService = connectivityService
+    }
+    
+    // MARK: -  public Method
+    func fetchWishlist() {
+        setIndicator(true)
+        networkManager.fetchData(from: ShopifyAPI.draftOrder(id: userDefaultsManager.wishlistID).shopifyURLString(), responseType: DraftOrderWrapper.self, headers: []) { result in
+            self.wishlist = result?.draftOrder.lineItems ?? []
+            if self.wishlist.first?.variantID == nil {
+                self.wishlist = Array(self.wishlist.dropFirst())
+            }
+            self.setIndicator(false)
+            if self.wishlist.count == 0 {
+                self.displayEmptyMessage("Add some products to your wishlist")
+            } else {
+                self.removeEmptyMessage()
+            }
+            self.bindResultToTableView()
+            
+        }
+    }
+}
